06914ef8d11497a947218eb0a61bfd44535d6776
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2023-08-19 22:46:20 +0500

    added get req for reminders

--- server.js
+++ server.js
@@ -75,154 +75,11 @@ cron.schedule('* * * * *', () => {
   console.log('Search complete');
 });
 
-// Handles incoming messages
-// app.post('/incoming', (req, res) => {
-//   console.log(req);
-//   const query = req.body.Body.split(' ');
-//   const clientNumber = extractClientNumber(req.body.From);
-//   const action = _.lowerCase(query[0]);
-
-//   // Creating reminders
-//   if (action === 'set') {
-//     // Send instructions
-//     if (!query[1]) {
-//       sendMessage(
-//         'Format: \n *set* _task(required, no spaces)_ _time(required, in HHMM format)_ _date(optional, in DD/MM format, default is today)_',
-//         res
-//       );
-//     } else {
-//       const taskName = query[1];
-//       const time = query[2];
-//       var hours = parseInt(time.slice(0, 2));
-//       var minutes = parseInt(time.slice(2, 4));
-//       var year = new Date().getUTCFullYear();
-
-//       // For today
-//       if (!query[3] || query[3] === 'today') {
-//         if (testInput(query)) {
-//           const istString = moment
-//             .tz(new Date().toISOString(), 'Asia/Kolkata')
-//             .format()
-//             .slice(0, 16);
-//           var month = istString.slice(5, 7);
-//           var date = istString.slice(8, 10);
-//           const isoString = new Date(
-//             year,
-//             month - 1,
-//             date,
-//             hours,
-//             minutes,
-//             0,
-//             0
-//           ).toISOString();
-//           const taskTime = isoString.slice(0, 16);
-//           console.log(`Reminder created for: ${taskTime}`);
-//           const taskInfo = new Reminder({
-//             taskName: taskName,
-//             taskTime: taskTime,
-//             taskTimeOG:
-//               new Date(year, month - 1, date, hours, minutes, 0, 0)
-//                 .toDateString()
-//                 .slice(0, 16) +
-//               ' at ' +
-//               new Date(year, month - 1, date, hours, minutes, 0, 0)
-//                 .toTimeString()
-//                 .slice(0, 5),
-//             clientNumber: clientNumber,
-//           });
-//           taskInfo.save((err) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               sendMessage(`Ok, will remind about *${taskName}*`, res);
-//             }
-//           });
-//         } else {
-//           sendMessage(
-//             "Please enter valid inputs and try again. Possible error: *Inputs not according to specified format* or *Reminder time given in past* (I hope you know time travel isn't possible yet)",
-//             res
-//           );
-//         }
-//       }
-
-//       // For any day
-//       else {
-//         if (testInput(query)) {
-//           const dateMonthString = query[3];
-//           var date = parseInt(dateMonthString.split('/')[0]);
-//           var month = parseInt(dateMonthString.split('/')[1]) - 1;
-//           const isoString = new Date(
-//             year,
-//             month,
-//             date,
-//             hours,
-//             minutes,
-//             0,
-//             0
-//           ).toISOString();
-//           const taskTime = isoString.slice(0, 16);
-//           console.log(`Reminder created for *${taskTime}*`);
-//           const taskInfo = new Reminder({
-//             taskName: taskName,
-//             taskTime: taskTime,
-//             taskTimeOG:
-//               new Date(year, month, date, hours, minutes, 0, 0)
-//                 .toDateString()
-//                 .slice(0, 16) +
-//               ' at ' +
-//               new Date(year, month, date, hours, minutes, 0, 0)
-//                 .toTimeString()
-//                 .slice(0, 5),
-//             clientNumber: clientNumber,
-//           });
-//           taskInfo.save((err) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               sendMessage(`Ok, will remind you about *${taskName}*`, res);
-//             }
-//           });
-//         } else {
-//           sendMessage(
-//             "Please enter valid inputs and try again. Possible error: *Inputs not according to specified format* or *Reminder time given in past* (I hope you know time travel isn't possible yet)",
-//             res
-//           );
-//         }
-//       }
-//     }
-//   }
-
-//   // View reminders
-//   else if (action === 'view') {
-//     console.log('view');
-//     Reminder.find({ clientNumber: clientNumber }, (err, foundTasks) => {
-//       if (err) {
-//         console.log(err);
-//       } else if (foundTasks.length) {
-//         const upcomingTasks = [];
-//         foundTasks.forEach((task) => {
-//           var subMessage = `*${task.taskName}* at *${task.taskTimeOG}*`;
-//           upcomingTasks.push(subMessage);
-//         });
-//         sendMessage(upcomingTasks.join('\n'), res);
-//       } else if (!foundTasks.length) {
-//         sendMessage(
-//           "You don't have any upcoming tasks. Create some first. To know how to create type *set* to get insight.",
-//           res
-//         );
-//       }
-//     });
-//   } else {
-//     sendMessage("I don't know what that means. Try *set* or *view*", res);
-//   }
-// });
-
 app.post('/incoming', (req, res) => {
-  const requestBody = req.body.Body.toLowerCase(); // Convert to lowercase for easier matching
+  const requestBody = req.body.Body.toLowerCase();
 
   if (requestBody.startsWith('cancel')) {
-    // Delete the record
-    const reminderToDelete = requestBody.substring(7).trim(); // Remove 'cancel' and trim
+    const reminderToDelete = requestBody.substring(7).trim();
     Reminder.deleteOne({ body: reminderToDelete }, (err) => {
       if (err) {
         console.error(err);
@@ -231,7 +88,6 @@ app.post('/incoming', (req, res) => {
       return res.status(200).json({ message: 'Reminder deleted successfully' });
     });
   } else {
-    // Parse and handle new reminder
     const dateRegex =
       /(?:next\s*)?([a-z]+)\s*(\d{1,2}(?:st|nd|rd|th)?)|(\d{1,2}(?:st|nd|rd|th)?)\s*([a-z]+)/;
     const timeRegex = /(\d{1,2}(?::\d{2})?\s*(?:am|pm?)?)/;
@@ -247,9 +103,7 @@ app.post('/incoming', (req, res) => {
     let [time] = timeMatch;
 
     if (!time.includes('am') && !time.includes('pm')) {
-      // If time doesn't specify am/pm, ask the user
-      // TODO: implement this as a prompt or user interaction
-      // For simplicity, we'll assume PM if no am/pm is provided
+      // Assume PM if no am/pm is provided
       time += 'pm';
     }
 
@@ -258,24 +112,23 @@ app.post('/incoming', (req, res) => {
       .replace(timeMatch[0], '')
       .trim();
 
-    const currentYear = new Date().getFullYear(); // Get the current year
+    const currentYear = new Date().getFullYear();
 
-    // Construct a date-time string in a format compatible with date-fns
-    const dateTimeStr = `${currentYear}-${month || monthAlt}-${
+    // Rearrange components for better parsing
+    const dateTimeStr = `${month || monthAlt} ${
       dayOfMonth || dayOfMonthAlt
-    }`;
-    // Create a Date object using the date-time string
-    // const dateTime = new Date(dateTimeStr);
-    const dateTime = parse(dateTimeStr, 'MMMM dd yyyy', new Date());
+    } ${currentYear} ${time}`;
 
-    // Check if the dateTime is valid and not in the past
+    // Fix the issue with 'dateTime' being null
+    const dateTime =
+      parse(dateTimeStr, 'MMMM dd yyyy h:mmaaa', new Date()) ?? Date.now();
+
     const now = new Date();
 
     if (dateTime <= now) {
       return res.status(400).json({ error: 'Invalid date and time' });
     }
 
-    // Create a new Reminder document and save it
     const newReminder = new Reminder({
       body: topic,
       dateTime: dateTime,
@@ -289,6 +142,54 @@ app.post('/incoming', (req, res) => {
       return res.status(200).json({ message: 'Reminder created successfully' });
     });
   }
+});
+
+app.get('/reminders', (req, res) => {
+  Reminder.find({}, (err, reminders) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    const fillerWords = ['remind', 'me', 'on', 'to', 'at'];
+
+    const markedReminders = reminders.map((reminder) => {
+      const words = reminder.body.toLowerCase().split(' ');
+
+      let date = '';
+      let time = '';
+      let topic = '';
+      let remainingWords = '';
+
+      for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        if (
+          word.endsWith('th') ||
+          word.endsWith('st') ||
+          word.endsWith('nd') ||
+          word.endsWith('rd')
+        ) {
+          date += word + ' ';
+        } else if (word.includes(':') || word.match(/^\d+(?:am|pm)$/)) {
+          time = word;
+          remainingWords = words.slice(i + 1).join(' ');
+          break;
+        } else if (!fillerWords.includes(word)) {
+          topic += word + ' ';
+        }
+      }
+
+      return {
+        date: date.trim(),
+        time,
+        topic: topic.trim(),
+        remainingWords,
+      };
+    });
+
+    return res.status(200).json({ reminders: markedReminders });
+  });
 });
 
 app.get('/', (req, res) => {
